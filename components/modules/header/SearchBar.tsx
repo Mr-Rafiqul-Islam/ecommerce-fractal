8d0f14203dd5b12400651a497e42e579cb43e7f1
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-11-01 09:45:37 +0600

    realted product show when search bar open complete

--- components/modules/header/SearchBar.tsx
+++ components/modules/header/SearchBar.tsx
@@ -12,6 +12,10 @@ import {
 import { Search, X } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import useSWR, { Fetcher } from "swr";
+import { Product } from "@/types";
+import Image from "next/image";
+import { getBestPriceWithDiscountFromProduct } from "@/lib/utils";
 
 export default function SearchBar({
   openSearchBar,
@@ -20,20 +24,56 @@ export default function SearchBar({
   openSearchBar: boolean;
   setOpenSearchBar: (v: boolean) => void;
 }) {
-    const handleSearch = () => { 
-        // todo call api
-     }
+
+    // client side data fethcing
+  const fetcher: Fetcher<Product[], string> = (...args) =>
+    fetch(...args)
+      .then((res) => res.json())
+      .then((data) => data.data);
+
+  const { data, error, isLoading } = useSWR<Product[]>(
+    process.env.NEXT_PUBLIC_API_URL + "/api/products",
+    fetcher
+  );
+  if (error)
+    return <div>Failed to load due to Categories data Fetching error!</div>;
+  console.log(data);
+  const handleSearch = () => {
+    // todo call api
+  };
   return (
-      <Dialog open={openSearchBar}>
-        <DialogContent className="lg:max-w-screen-xl z-[9999] [&>.closeBtn]:hidden ">
-          <div className="flex items-center w-full gap-4">
-            <Search className="text-slate-300"/>
-            <Input placeholder="type any product here" onInput={handleSearch} className=" text-slate-500"/>
-            <Button onClick={() => setOpenSearchBar(false)} variant="outline" size={"icon"} className="hover:bg-primary-500 group">
-            <X className="group-hover:text-white"/>
-            </Button>
-          </div>
-        </DialogContent>
-      </Dialog>
+    <Dialog open={openSearchBar}>
+      <DialogContent className="lg:max-w-screen-xl z-[9999] [&>.closeBtn]:hidden ">
+        <div className="flex items-center w-full gap-4">
+          <Search className="text-slate-300" />
+          <Input
+            placeholder="type any product here"
+            onInput={handleSearch}
+            className=" text-slate-500"
+          />
+          <Button
+            onClick={() => setOpenSearchBar(false)}
+            variant="outline"
+            size={"icon"}
+            className="hover:bg-primary-500 group"
+          >
+            <X className="group-hover:text-white" />
+          </Button>
+        </div>
+        <div className="flex overflow-y-auto w-full py-12 gap-12 flex-col justify-start">
+          {
+            data && data.map((item: Product) => (
+              <div
+                key={item._id}
+                className="w-full flex flex-col justify-start gap-8 px-8 items-center"
+              >
+                <Image src={item.subProducts[0].options[0].images[0]} alt={item.name} width={60} height={80} className="object-contain"/>
+                <h6>{getBestPriceWithDiscountFromProduct(item)}</h6>
+              </div>
+            ))
+          }
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
--- types/index.ts
+++ types/index.ts
@@ -1,33 +1,78 @@
 // All Types here
 
-export type Category={
-    _id:string;
+export type Category = {
+  _id: string;
+  name: string;
+  link: string;
+  slug: string;
+  image: string;
+  createdAt?: string;
+  submenu?: SubCategory[];
+};
+export type SubCategory = {
+  name: string;
+  slug: string;
+  link: string;
+  parent: string;
+};
+export type Page = {
+  _id: string;
+  name: string;
+  slug: string;
+  link: string;
+  createdAt?: string;
+  subPage?: SubPage[];
+};
+export type SubPage = {
+  _id: string;
+  name: string;
+  slug: string;
+  link: string;
+  createdAt?: string;
+  parent?: string;
+};
+export type Product = {
+  _id: string;
+  name: string;
+  slug: string;
+  feature: boolean;
+  description: string;
+  category: Category;
+  subCategories: SubCategory[];
+  brand: Brand;
+  content: string;
+  details: Detail[];
+  questions: Question[];
+  reviews: Review[];
+  subProducts: SubProduct[];
+};
+export type Detail = {};
+export type Brand = {};
+export type Question = {};
+export type Review = {
+    rating:number;
+};
+export type Style = {
     name:string;
-    link:string;
-    slug:string;
+    color:string;
     image:string;
-    createdAt?:string;
-    submenu?:SubCategory[];
-}
-export type SubCategory={
-    name:string;
-    slug:string;
-    link:string;
-    parent:string;   
-}
-export type Page={
+};
+export type Images ={};
+
+export type Options = {
+    images:string[];
+    option:string;
+    qty:number;
+    price:number;
+    discount:number;
     _id:string;
-    name:string;
-    slug:string;
-    link:string;
-    createdAt?:string;
-    subPage?:SubPage[];
-}
-export type SubPage={
-    _id:string;
-    name:string;
-    slug:string;
-    link:string;
-    createdAt?:string;
-    parent?:string;   
-}
+    sold:number;
+    
+};
+
+export type SubProduct = {
+  sku: string;
+  style: Style;
+  options: Options[];
+  _id: string;
+};
