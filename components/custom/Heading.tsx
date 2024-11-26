eb08d170c08eb23209a579678426cf1f66df00dc
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-11-26 15:08:53 +0600

    heading border added

--- components/custom/Heading.tsx
+++ components/custom/Heading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Heading({name}:{name:string}) {
   return (
-    <div className='flex w-full relative'>
-        <h1 className='after:w-[80px] after:h-[2px] after:bg-primary-700 pb-2 after:absolute after:left-0 after:bottom-0 text-xl lg:text-3xl font-bold capitalize border-b border-b-gray-300'>
+    <div className='flex w-full relative border-b border-b-gray-300'>
+        <h1 className='after:w-[80px] after:h-[2px] after:bg-primary-700 pb-2 after:absolute after:left-0 after:bottom-0 text-xl lg:text-3xl font-bold capitalize '>
         {name}
         </h1>
     </div>
