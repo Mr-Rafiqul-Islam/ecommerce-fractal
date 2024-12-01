a73a0d14cc7d442b28738f31ffa45e3b668515d8
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-12-01 18:38:51 +0600

    sort Filter Set

---
+++ components/custom/MobileSidebarLeft.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MobileSidebarLeft() {
+  return (
+    <div>MobileSidebarLeft</div>
+  )
+}
--- components/custom/ProductTopBar.tsx
+++ components/custom/ProductTopBar.tsx
@@ -1,40 +1,92 @@
-import { Product } from '@/types';
-import React from 'react'
+import { Product } from "@/types";
+import React from "react";
+import MobileSidebarLeft from "./MobileSidebarLeft";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 export default function ProductTopBar({
-    minPrice,
-    setMinPrice,
-    maxPrice,
-    setMaxPrice,
-    loading,
-    setLoading,
-    perpages,
-    setPerpages,
-    filter,
-    setFilter,
-    page,
-    maxPage,
-    products,
-  }: {
-    minPrice: number;
-    setMinPrice: (v: number) => void;
-    maxPrice: number;
-    setMaxPrice: (v: number) => void;
-    loading: boolean;
-    setLoading: (v: boolean) => void;
-    perpages: number;
-    setPerpages: (v: number) => void;
-    filter: string;
-    setFilter: (v: string) => void;
-    page: number;
-    maxPage: number;
-    products: Product[];
-  }) {
+  minPrice,
+  setMinPrice,
+  maxPrice,
+  setMaxPrice,
+  loading,
+  setLoading,
+  perpages,
+  setPerpages,
+  filter,
+  setFilter,
+  page,
+  maxPage,
+  products,
+}: {
+  minPrice: number;
+  setMinPrice: (v: number) => void;
+  maxPrice: number;
+  setMaxPrice: (v: number) => void;
+  loading: boolean;
+  setLoading: (v: boolean) => void;
+  perpages: number;
+  setPerpages: (v: number) => void;
+  filter: string;
+  setFilter: (v: string) => void;
+  page: number;
+  maxPage: number;
+  products: Product[];
+}) {
   return (
-    <div className='lg:flex items-center justify-between w-full '>
-        <div className="flex items-center justify-between gap-4 flex-1 w-full">
-            
+    <div className="lg:flex items-center justify-between w-full ">
+      <div className="flex items-center justify-between gap-4 flex-1 w-full">
+        <MobileSidebarLeft />
+        <div className="hidden lg:block">
+          Showing {maxPage === page ? products.length : perpages * page} of{" "}
+          {products.length} results
         </div>
+        <div className="ms-auto">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Filter
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup value={"bottom"}>
+                <DropdownMenuRadioItem
+                  value="top"
+                  onClick={() => setFilter("alphabetic")}
+                >
+                  Alphabetic
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem
+                  value={filter}
+                  onClick={() => setFilter("priceLowToHigh")}
+                >
+                  Price: Low to high
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem
+                  value={filter}
+                  onClick={() => setFilter("priceHighToLow")}
+                >
+                  Price : High to low
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem
+                  value={filter}
+                  onClick={() => setFilter("latest")}
+                >
+                  Latest
+                </DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
